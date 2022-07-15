4fd7faa7188a09c502174659e69902d7b375ae3a
Ezra-code <njerukithinji872@gmail.com>
2022-07-15 18:03:35 +0300

    static web page using react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from './components/Main';
+import NavBar from "./components/Navbar";
+import "./style.css"
+
+function App() {
+    return (
+        <div>
+            <NavBar />
+            <Main />
+        </div>
+     
+    )
+}
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <nav className="nav">
+            <h1 className="left"><i class="fa-brands fa-react"></i>React Facts</h1>
+            <h3 className="right">React Course-Project Uno</h3>
+        </nav>
+    )
+}
+export default NavBar
